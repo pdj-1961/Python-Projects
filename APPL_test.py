9b4a04407d58edbad3e889b1340347f032a73296
Peter de Jong <pdj-1961@outlook.com>
2025-05-24 17:53:32 +0200

    Update with path definition

--- APPL_test.py
+++ APPL_test.py
@@ -1,5 +1,14 @@
+from pathlib import PureWindowsPath
 import pandas as pd
 
-APPL = pd.read_csv('HistoricalDataAAPL.csv')
+# This script reads a CSV file containing historical data for Apple Inc. (AAPL) stock
+# and prints the first few rows of the DataFrame.
 
+# Define the path to the CSV file using PureWindowsPath
+AAPL_path = PureWindowsPath('c:/', 'Users', 'PdJ-1', 'Documents', 'GitHub', 'Python-Projects', 'HistoricalDataAAPL.csv')
+
+# Read the CSV file into a DataFrame
+APPL = pd.read_csv(AAPL_path)
+
+# Display the first few rows of the DataFrame
 print(APPL.head())
